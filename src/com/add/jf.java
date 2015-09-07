9b9a89c447ceb037835781782266a205b05b10a6
wangpengfei <wangpengfei@fly>
2015-09-07 19:46:15 +0800

    项目提交

---
+++ src/com/add/jf.java
@@ -0,0 +1,9 @@
+package com.add;
+
+public class jf {
+	public int sum(int a,int b) {
+		int resultSum=a+b;
+		return resultSum;
+	}
+
+}
---
+++ src/com/add/jfTest.java
@@ -0,0 +1,14 @@
+package com.add;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class jfTest {
+
+	@Test
+	public void testSum(){
+	fail("Not yet implemented");
+	}
+
+}
